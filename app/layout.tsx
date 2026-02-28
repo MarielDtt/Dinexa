9f267549d9a5cc87d1569de34148abb12dc99fc2
MarielDtt <marieldruetta@gmail.com>
2026-02-27 23:40:58 -0300

    feat: implementar conexión de Crisp Chat en el layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ThemeRegistry from "@/theme/ThemeRegistry";
 import Navbar from "./components/layout/Navbar";
-import { Poppins } from 'next/font/google';
+import { Poppins } from "next/font/google";
 import Footer from "./components/layout/Footer";
-import ChatButton from "./components/ui/ChatButton";
-
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,6 +19,7 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
   },
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,19 +27,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-
       <body className={`antialiased ${poppins.variable} font-sans`}>
         <div className="min-h-screen bg-background-default text-text-primary">
-
           <ThemeRegistry>
             <Navbar />
             {children}
             <Footer />
-            <ChatButton />
+
+            <Script
+              id="crisp-chat"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.$crisp = window.$crisp || [];
+                  window.CRISP_WEBSITE_ID = "f9d5cff0-6256-4460-9618-f803a41fbde2";
+                  (function() {
+                    var d = document;
+                    var s = d.createElement("script");
+                    s.src = "https://client.crisp.chat/l.js";
+                    s.async = 1;
+                    d.getElementsByTagName("head")[0].appendChild(s);
+                  })();
+                `,
+              }}
+            />
           </ThemeRegistry>
         </div>
       </body>
-
     </html>
   );
 }
